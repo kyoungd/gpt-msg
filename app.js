2615ae75470c9809e21f9b85b8f65bd65fd1559e
kyoungd <kyoungd@hotmail.com>
2023-02-19 20:30:19 -0800

    went back to express

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile('index.html', 'utf8', (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    });
-  } 
-  if (req.url.endsWith('.js')) {
-    fs.readFile('.' + req.url, 'utf8', (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.end(data);
-    });
-  }
-  if (req.url.endsWith('.css')) {
-    fs.readFile('.' + req.url, 'utf8', (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/css'});
-      res.end(data);
-    });
-  }
-});
+// Serve static files from the public directory
+app.use(express.static('public'));
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
